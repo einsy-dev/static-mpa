25124b15f416a900d8ac45efa9c50aca53bd968a
Einsy <eins.yeager@gmail.com>
2024-02-26 23:18:33 +0600

    start optimizing style with screen width and adding placeholder footer, also upgrading swipe to ignore some swipe angles

--- src/ts/utils/swipe.ts
+++ src/ts/utils/swipe.ts
@@ -1,11 +1,15 @@
-export default function swipeEvent(el: any, callback: any) {
+export default function swipeEvent(el: any, callback: any, ignoreValue = 100) {
   let startX = 0;
   let endX = 0;
+  let startY = 0;
+  let endY = 0;
+  let ignore;
 
   el.addEventListener(
     "touchstart",
     (e: TouchEvent) => {
       startX = e.touches[0].clientX;
+      startY = e.touches[0].clientY;
     },
     false
   );
@@ -14,7 +18,13 @@ export default function swipeEvent(el: any, callback: any) {
     "touchend",
     (e: TouchEvent) => {
       endX = e.changedTouches[0].clientX;
+      endY = e.changedTouches[0].clientY;
 
+      startY > endY
+        ? (ignore = startY - endY < ignoreValue)
+        : (ignore = startY - endY > -ignoreValue);
+
+      if (!ignore) return;
       if (endX < startX) {
         callback(false);
       }
